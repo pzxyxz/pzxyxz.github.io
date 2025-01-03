9cab1726e552d02bfef7941594a3ab32f581df58
pzxyxz <preisha.mishra@rutgers.edu>
2025-01-03 17:16:09 -0500

    js themes update

--- assets/js/dark_mode.js
+++ assets/js/dark_mode.js
@@ -1,7 +1,12 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const mode_toggle = document.getElementById("light-toggle");
+document.addEventListener("DOMContentLoaded", () => {
+  const toggle = document.querySelector("#theme-toggle");
+  toggle.addEventListener("click", () => {
+    const currentTheme = document.documentElement.getAttribute("data-theme");
+    const newTheme = currentTheme === "dark" ? "light" : "dark";
+    document.documentElement.setAttribute("data-theme", newTheme);
+    localStorage.setItem("theme", newTheme);
+  });
 
-    mode_toggle.addEventListener("click", function() {
-        toggleTheme(localStorage.getItem("theme"));
-    });
+  const savedTheme = localStorage.getItem("theme") || "light";
+  document.documentElement.setAttribute("data-theme", savedTheme);
 });
