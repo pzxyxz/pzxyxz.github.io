c0d15cc10aa9420bd092abb0949df7257a407989
pzxyxz <preisha.mishra@rutgers.edu>
2025-01-11 21:13:17 -0500

    update purge

--- purgecss.config.js
+++ purgecss.config.js
@@ -1,6 +1,13 @@
 module.exports = {
-  content: ["_site/**/*.html", "_site/**/*.js"],
-  css: ["_site/assets/css/*.css"],
+  content: [
+    "file:///C:/Users/preis/Desktop/portfolio/pzxyxz.github.io/_site/**/*.html",
+    "file:///C:/Users/preis/Desktop/portfolio/pzxyxz.github.io/_site/**/*.js"
+  ],
+  css: [
+    "file:///C:/Users/preis/Desktop/portfolio/pzxyxz.github.io/_site/assets/css/*.css"
+  ],
   output: "_site/assets/css/",
-  skippedContentGlobs: ["_site/assets/**/*.html"],
+  skippedContentGlobs: [
+    "file:///C:/Users/preis/Desktop/portfolio/pzxyxz.github.io/_site/assets/**/*.html"
+  ],
 };
